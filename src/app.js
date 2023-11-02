78230888bc328e32fdeaf0e8ff5f361ef111b785
JhobanyT <jhobanyticona@gmail.com>
2023-11-02 12:49:15 -0500

    actualización seguridad

--- src/app.js
+++ src/app.js
@@ -5,17 +5,28 @@ const insRouter = require('./routes/insRouter.js');
 const estudianteRouter = require('./routes/estudianteRouter.js');
 const certificadoRouter = require('./routes/certificadoRouter.js');
 const nivelAcademicoRouter = require('./routes/nivelAcademicoRouter.js');
-
 const app = express();
 
+// Configurar encabezados de seguridad
+app.use((req, res, next) => {
+  res.setHeader('X-Content-Type-Options', 'nosniff');
+  res.setHeader('X-Frame-Options', 'DENY');
+  res.setHeader('X-XSS-Protection', '1; mode=block');
+  res.setHeader('Content-Security-Policy', "default-src 'self'");
+  next();
+});
+
+// Manejar errores de forma segura
+app.use((err, req, res, next) => {
+  console.error(err);
+  res.status(500).json({ error: 'Error interno del servidor' });
+});
 
 app.use(express.json());
-
 app.use('/api', usuarioRouter);
 app.use('/api', documentoRouter);
 app.use('/api', insRouter);
 app.use('/api', estudianteRouter);
 app.use('/api', certificadoRouter);
 app.use('/api', nivelAcademicoRouter);
-
 module.exports = app;
--- src/config/db.js
+++ src/config/db.js
@@ -1,10 +1,12 @@
+require('dotenv').config(); // Cargar las variables de entorno desde .env
+
 const mysql = require('mysql2');
 
 const db = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '123456',
-  database: 'dbcerty'
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
 });
 
 module.exports = db;
